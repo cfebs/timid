3765391d5d991ed96819fdc78e7feae663f9b29c
Collin Lefeber <collin@vimeo.com>
2020-08-27 22:15:11 -0400

    Basic timer

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,25 @@
 <script>
-	let name = 'world';
+    const VIEW_TIMER = 0;
+    const VIEW_LOG = 1;
+    const VIEW_OPTIONS = 2;
+
+    let CURRENT_VIEW = VIEW_TIMER;
+    import Timer from './Timer.svelte';
+
+    function setView(event) {
+        console.log(arguments);
+        CURRENT_VIEW = type;
+    }
 </script>
 
-<h1>Hello {name}!</h1>
+<h1>Work timer</h1>
+
+<nav>
+    <ul>
+        <li><a href="#timer">Timer</a></li>
+        <li><a href="#log">Log</a></li>
+        <li><a href="#options">Options</a></li>
+    </ul>
+</nav>
+
+<Timer />
---
+++ src/Timer.svelte
@@ -0,0 +1,105 @@
+<script>
+    const COUNTDOWN_STATE_STOP = 0;
+    const COUNTDOWN_STATE_PAUSE = 1;
+    const COUNTDOWN_STATE_RUN = 2;
+    const COUNTDOWN_STATE_WAITING = 3;
+
+    const COUNTDOWN_TYPE_WORK = 0;
+    const COUNTDOWN_TYPE_BREAK = 1;
+
+    const breakSeconds = 5 * 60;
+    const workSeconds = 35 * 60;
+
+    let COUNTDOWN_TYPE = COUNTDOWN_TYPE_WORK;
+    let COUNTDOWN_STATE = COUNTDOWN_STATE_STOP;
+
+    let countdownSeconds = workSeconds;
+    let remainingSeconds = countdownSeconds;
+    let timerInterval;
+    $: remainingSecondsInTime = secondsToTime(remainingSeconds)
+
+    function padZeros(n) {
+        if (n >= 10) {
+            return '' + n;
+        }
+
+        return '0' + n;
+    }
+
+    function secondsToTime(seconds) {
+        const hours = Math.floor(seconds / 3600);
+        seconds %= 3600;
+        const minutes = Math.floor(seconds / 60);
+        const s = seconds % 60;
+        return `${padZeros(hours)}:${padZeros(minutes)}:${padZeros(s)}`
+    }
+
+    function startTimer() {
+        clearInterval(timerInterval);
+
+        if (COUNTDOWN_STATE !== COUNTDOWN_STATE_PAUSE) {
+            remainingSeconds = countdownSeconds;
+        }
+
+        COUNTDOWN_STATE = COUNTDOWN_STATE_RUN
+        timerInterval = setInterval(() => {
+            remainingSeconds -= 1;
+            if (remainingSeconds === 0) {
+                clearInterval(timerInterval);
+                nextStage();
+            }
+        }, 1000);
+    }
+
+    function pause() {
+        clearInterval(timerInterval);
+        COUNTDOWN_STATE = COUNTDOWN_STATE_PAUSE;
+    }
+
+    function nextStage() {
+        // always go into waiting stage next
+        if (COUNTDOWN_TYPE !== COUNTDOWN_STATE_WAITING) {
+            COUNTDOWN_TYPE = COUNTDOWN_STATE_WAITING
+            return;
+        }
+    }
+
+    function skipStage() {
+
+    }
+</script>
+<style>
+    .timer {
+        width: 800px;
+        text-align: center;
+        margin: 0 auto;
+    }
+    textarea {
+        width: 80%;
+        display: block;
+        margin: 1em auto;
+    }
+    time {
+        font-size: 15vh;
+    }
+</style>
+
+<div class="timer">
+    <time datetime={remainingSecondsInTime}>
+        {remainingSecondsInTime}
+    </time>
+
+    <textarea placeholder="Notes for this session" rows={5}></textarea>
+
+    <button on:click={startTimer}>
+        {#if COUNTDOWN_STATE === COUNTDOWN_STATE_PAUSE}
+            Resume timer
+        {:else if COUNTDOWN_STATE === COUNTDOWN_STATE_RUN}
+            Restart timer
+        {:else}
+            Start Timer
+        {/if}
+    </button>
+    <button on:click={skipStage}>Skip stage</button>
+    <button on:click={pause} disabled={COUNTDOWN_STATE !== COUNTDOWN_STATE_RUN}>Pause</button>
+</div>
