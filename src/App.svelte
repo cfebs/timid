a14f9beac4fd781145e272afd617690f78d526dd
Collin Lefeber <collin@vimeo.com>
2020-08-27 23:34:15 -0400

    Dark mode

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@
     const VIEW_LOG = 1;
     const VIEW_OPTIONS = 2;
 
+    let DARK_MODE = true;
+
     let CURRENT_VIEW = VIEW_TIMER;
     import Timer from './Timer.svelte';
 
@@ -10,7 +12,27 @@
         console.log(arguments);
         CURRENT_VIEW = type;
     }
+
+    function toggleColor() {
+        window.document.body.classList.toggle('light');
+        DARK_MODE = !DARK_MODE;
+    }
 </script>
+
+<style>
+:root {
+    --bg-color: #000000;
+    --text-color: #FFFFFF;
+}
+:global(body, textarea, button, input) {
+    background: var(--bg-color);
+    color: var(--text-color);
+}
+:global(body.light) {
+    --bg-color: #FFFFFF;
+    --text-color: #000000;
+}
+</style>
 
 <h1>Work timer</h1>
 
@@ -19,6 +41,7 @@
         <li><a href="#timer">Timer</a></li>
         <li><a href="#log">Log</a></li>
         <li><a href="#options">Options</a></li>
+        <li><a href="#" on:click={toggleColor}>{ DARK_MODE ? 'Light mode' : 'Dark mode' }</a></li>
     </ul>
 </nav>
 
