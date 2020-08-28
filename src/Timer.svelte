11c517cd1b687dcc583972cc66884bb46141fdae
Collin Lefeber <collin@vimeo.com>
2020-08-28 11:41:58 -0400

    Stages and notifications

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -9,6 +9,8 @@
 
     const breakSeconds = 5 * 60;
     const workSeconds = 35 * 60;
+    // const breakSeconds = 5;
+    // const workSeconds = 10;
 
     let COUNTDOWN_TYPE = COUNTDOWN_TYPE_WORK;
     let COUNTDOWN_STATE = COUNTDOWN_STATE_STOP;
@@ -16,6 +18,7 @@
     let countdownSeconds = workSeconds;
     let remainingSeconds = countdownSeconds;
     let timerInterval;
+    let waitInterval;
     $: remainingSecondsInTime = secondsToTime(remainingSeconds)
 
     function padZeros(n) {
@@ -36,18 +39,20 @@
 
     function startTimer() {
         clearInterval(timerInterval);
+        clearInterval(waitInterval);
 
         if (COUNTDOWN_STATE !== COUNTDOWN_STATE_PAUSE) {
             remainingSeconds = countdownSeconds;
         }
 
         COUNTDOWN_STATE = COUNTDOWN_STATE_RUN
+        remainingSeconds -= 1;
         timerInterval = setInterval(() => {
             remainingSeconds -= 1;
             if (remainingSeconds === 0) {
                 clearInterval(timerInterval);
-                COUNTDOWN_TYPE = COUNTDOWN_STATE_WAITING;
-                notify();
+                COUNTDOWN_STATE = COUNTDOWN_STATE_WAITING;
+                wait()
             }
         }, 1000);
     }
@@ -57,14 +62,48 @@
         COUNTDOWN_STATE = COUNTDOWN_STATE_PAUSE;
     }
 
-    function notify() {
+    function notify(msg) {
         if (Notification.permission === "granted") {
-            var notification = new Notification("Done");
+            var notification = new Notification(msg);
+            return notification;
         }
     }
 
-    function skipStage() {
+    function wait() {
+        clearInterval(waitInterval)
+        let msg = ""
 
+        if (COUNTDOWN_TYPE === COUNTDOWN_TYPE_BREAK) {
+            msg = "Timer: Break done! Waiting to start work stage.";
+        }
+        else if (COUNTDOWN_TYPE === COUNTDOWN_TYPE_WORK) {
+            msg = "Timer: Work done! Waiting to start break stage.";
+        }
+
+        notify(msg);
+        waitInterval = setInterval(() => {
+            notify(msg);
+        }, 15000);
+    }
+
+    function nextStage() {
+        if (COUNTDOWN_TYPE === COUNTDOWN_TYPE_WORK) {
+            console.log('Going into break for', breakSeconds);
+            countdownSeconds = breakSeconds;
+            COUNTDOWN_TYPE = COUNTDOWN_TYPE_BREAK;
+            startTimer();
+            return;
+        }
+
+        if (COUNTDOWN_TYPE === COUNTDOWN_TYPE_BREAK) {
+            console.log('Going into work for', workSeconds);
+            countdownSeconds = workSeconds;
+            COUNTDOWN_TYPE = COUNTDOWN_TYPE_WORK;
+            startTimer();
+            return;
+        }
+
+        throw new Error('Invalid stage state');
     }
 </script>
 <style>
@@ -89,6 +128,24 @@
         {remainingSecondsInTime}
     </time>
 
+    <div>
+        {#if COUNTDOWN_TYPE === COUNTDOWN_TYPE_WORK}
+            Work
+        {:else if COUNTDOWN_TYPE === COUNTDOWN_TYPE_BREAK}
+            Break
+        {/if}
+        -
+        {#if COUNTDOWN_STATE === COUNTDOWN_STATE_PAUSE}
+            Paused
+        {:else if COUNTDOWN_STATE === COUNTDOWN_STATE_RUN}
+            Running
+        {:else if COUNTDOWN_STATE === COUNTDOWN_STATE_WAITING}
+            Waiting
+        {:else if COUNTDOWN_STATE === COUNTDOWN_STATE_STOP}
+            Waiting
+        {/if}
+    </div>
+
     <textarea placeholder="Notes for this session" rows={5}></textarea>
 
     <button on:click={startTimer}>
@@ -100,6 +157,12 @@
             Start Timer
         {/if}
     </button>
-    <button on:click={skipStage}>Skip stage</button>
+    <button on:click={nextStage}>
+        {#if COUNTDOWN_STATE === COUNTDOWN_STATE_WAITING}
+            Next stage
+        {:else}
+            Skip stage
+        {/if}
+    </button>
     <button on:click={pause} disabled={COUNTDOWN_STATE !== COUNTDOWN_STATE_RUN}>Pause</button>
 </div>
